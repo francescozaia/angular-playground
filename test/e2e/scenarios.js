700a0a3f8caa0296997fde5e0ebd6d6fd533c811
Francesco Zaia <francesco.zaia@gmail.com>
2014-06-15 15:33:35 +0100

    Cleaned up folder tree

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,42 +1,67 @@
 'use strict';
 
+var fs = require('fs');
+
+// abstract writing screen shot to a file
+function writeScreenShot(data, filename) {
+    var stream = fs.createWriteStream(filename);
+
+    stream.write(new Buffer(data, 'base64'));
+    stream.end();
+}
+
 /* https://github.com/angular/protractor/blob/master/docs/getting-started.md */
 
 describe('my app', function() {
 
-  browser.get('index.html');
+    browser.get('index.html');
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
-  });
-
-
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view1');
+    it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+        expect(browser.getLocationAbsUrl()).toMatch("/view1");
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+    describe('view1', function() {
+
+        beforeEach(function() {
+            browser.get('index.html#/view1');
+        });
+
+
+        it('should render view1 when user navigates to /view1', function() {
+            expect(element.all(by.css('[ng-view] p')).first().getText()).
+                toMatch(/partial for view 1/);
+        });
+
     });
 
-  });
+
+    describe('view2', function() {
+
+        beforeEach(function() {
+            browser.get('index.html#/view2');
+        });
 
 
-  describe('view2', function() {
+        it('should render view2 when user navigates to /view2', function() {
+            expect(element.all(by.css('[ng-view] p')).first().getText()).
+                toMatch(/partial for view 2/);
+        });
 
-    beforeEach(function() {
-      browser.get('index.html#/view2');
     });
 
+    describe('Pricing page', function() {
 
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
+        beforeEach(function() {
+            browser.get('pricing.html');
+        });
+
+
+        it('should render the pricing page', function() {
+            browser.takeScreenshot().then(function (png) {
+                writeScreenShot(png, 'exception.png');
+            });
+        });
+
     });
-
-  });
 });
