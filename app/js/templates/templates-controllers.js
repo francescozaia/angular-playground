37e78543605c75494b8bd1673982d6d58ccc64d3
Francesco Zaia <francesco.zaia@gmail.com>
2014-06-22 18:28:40 +0200

    improved ng-infinitescroll

--- app/js/templates/templates-controllers.js
+++ app/js/templates/templates-controllers.js
@@ -3,7 +3,7 @@
 angular.module('Templates.controllers', []).
     controller('templatesController', function($scope, templatesAPIService) {
         templatesAPIService.getPackages().success(function (response) {
-            var images = 6;
+            var images = 12;
             $scope.templates = response.templates.slice(0, images);
             $scope.loadMore = function() {
                 var last = $scope.templates.length - 1;
