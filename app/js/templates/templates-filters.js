b8b54673218bd671bd4ec892536375f2941b9da0
Francesco Zaia <francesco.zaia@gmail.com>
2014-07-19 21:34:08 +0100

    proper Angular filter

--- app/js/templates/templates-filters.js
+++ app/js/templates/templates-filters.js
@@ -8,4 +8,13 @@ angular.module('Templates.filters',[])
             //x = (input === condition) ? 'Yo' : 'no';
             return Boolean(input.indexOf(condition) > -1) ? 'block' : 'none';
         };
+    })
+    .filter('yoFilterTwo', function($filter) {
+        return function (list, arrayFilter) {
+            if (arrayFilter) {
+                return $filter("filter")(list, function (listItem) {
+                    return listItem.tags.indexOf(arrayFilter) != -1;
+                });
+            }
+        };
     });
