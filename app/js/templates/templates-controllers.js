ebe910e81469d0b785163169f9b3afbdfbe206fc
Francesco Zaia <fzaia@moonfruit.com>
2014-07-21 09:29:55 +0100

    fixes

--- app/js/templates/templates-controllers.js
+++ app/js/templates/templates-controllers.js
@@ -3,7 +3,7 @@
 angular.module('Templates.controllers', []).
     controller('templatesController', function($scope, templatesAPIService) {
         templatesAPIService.getPackages().success(function (response) {
-            $scope.gatto = "all";
+            $scope.tags = "all";
             $scope.templates = response.templates;
             $scope.templateTags = function () {
                 var arrays = _.map($scope.templates, function(template) {
